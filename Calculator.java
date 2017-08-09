66a818ff247a84846750445ebbc61a6225e8000f
ja-ho <magicuv@gmail.com>
2017-08-09 14:38:25 +0900

    teach version

--- Calculator.java
+++ Calculator.java
@@ -2,17 +2,7 @@ package com.mycompany.test;
 //git test
 public class Calculator {
   public static void main(String[] args) {
-    int a = 4;
-    int b = 4;
-    System.out.println( sum(a,b) );
-    system.out.println( minus(a,b) );
   }
 
-  private static int sum(int a, int b) {
-    return a + b;
-  }
 
-  private static int minus(int a, int b) {
-    return a - b;
-  }
 }

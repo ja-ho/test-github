1c5b4bba855cefdfb246fc60733ef84d6c39d9f2
ja-ho <magicuv@gmail.com>
2017-08-09 14:53:10 +0900

    test merge

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,10 @@ public class Calculator {
   public static void main(String[] args) {
   }
 
+  private static int sum(int a, int b) {
+    return a + b;
+  }
+
   private static int minus(int a, int b, Object GPU) {
     int result = 0;
     if( GPU == NULL ) {
